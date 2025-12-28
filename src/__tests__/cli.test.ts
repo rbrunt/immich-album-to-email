31fe89a7fd0ea6299bdcf7eda4331455c1bf9dd7
Richard Brunt <1533730+rbrunt@users.noreply.github.com>
2025-12-28 07:43:04 +0000

    Add and run prettier

--- src/__tests__/cli.test.ts
+++ src/__tests__/cli.test.ts
@@ -1,9 +1,8 @@
-
 describe("test test", () => {
-
-	it("passes a passing test", () =>{
-		expect(true).toBeTruthy();
-})
-	it("fails a failing test", () => {
-expect(false).toBeTruthy();})
-})
+  it("passes a passing test", () => {
+    expect(true).toBeTruthy();
+  });
+  it("fails a failing test", () => {
+    expect(false).toBeTruthy();
+  });
+});
